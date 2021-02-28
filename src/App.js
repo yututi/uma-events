95259112c9b35aefc7080213ebc2f9cbd8a6cf1a
yututi <y.tsuchiya0207@gmail.com>
2021-02-28 23:12:00 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import {useEffect, useState, memo} from "react"
 import './App.css';
 
-function App() {
+let events = []
+
+const App = () => {
+
+  const [isInit, setIsInit] = useState(false)
+  const [suggests, setSuggests] = useState([])
+
+  useEffect(() => {
+    fetch("./supports.json")
+    .then(res=> res.text())
+    .then(text=> JSON.parse(text))
+    .then(_supports => {
+      events = _supports.reduce((acc, cur) => {
+        cur.events.forEach(event => {
+          acc.push({
+            supportName: cur.name,
+            eventName: event.name,
+            choices: event.choices
+          })
+        })
+        return acc
+      }, [])
+      setIsInit(true)
+    })
+  }, [])
+
+  const onInput = e => {
+    const val = e.currentTarget.value
+    if(!val) {
+      setSuggests([])
+      return
+    }
+    const matched = events.reduce((acc, cur) => {
+      if(cur.eventName.indexOf(val) > -1){
+        acc.push(cur)
+      }
+      return acc
+    }, [])
+    setSuggests(matched)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isInit?<input type="text" onInput={onInput}></input>:""}
+      <SuggestedEvents events={suggests}></SuggestedEvents>
     </div>
   );
 }
+
+const SuggestedEvents = memo(({events}) => {
+  return (
+    <div class="suggestions">
+    {events.map(event => {
+      return (
+        <>
+          <h2>{event.eventName}</h2>
+          <h5>{event.supportName}</h5>
+          <ul>
+          {event.choices.map(choice => {
+            return (
+              <>
+                <li>{choice.name}</li>
+                <div>{choice.detail}</div>
+              </>
+            )
+          })}
+          </ul>
+        </>
+      )
+    })}
+    </div>
+  )
+})
 
 export default App;
